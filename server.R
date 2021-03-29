a71c1cf9b1dd3a3c3594d1e0d75533640ea395fd
msyphus <mark.syphus@gmail.com>
2021-03-29 15:23:27 -0600

    separated app file to fix deployment error

--- server.R
+++ server.R
@@ -39,9 +39,9 @@ docs<-VCorpus(VectorSource(c(resume, foodWaste, product, success, fishing, lean)
 
 # Clean up text: remove special characters, convert case, remove numbers, remove stopwords, etc.
 specialCharacters<-content_transformer(
-	function(x, pattern)
-		gsub(pattern, " ", x)
-	)
+  function(x, pattern)
+    gsub(pattern, " ", x)
+)
 docs<-tm_map(docs, specialCharacters, c("-", "%", "&"))
 docs<-tm_map(docs, removeNumbers)
 docs<-tm_map(docs, removePunctuation)
@@ -61,45 +61,15 @@ hist(docsDf$freq)
 # Get sentiment data
 emo<-get_nrc_sentiment(c(resume, foodWaste, product, success, fishing, lean))
 
-# Create Dashboard
-
-ui<-dashboardPage(
-	dashboardHeader(title = "Mark Syphus"),
-	dashboardSidebar(
-		sidebarMenu(
-			menuItem("Overall Sentiment", tabName="overall", icon=icon("dashboard")),
-			menuItem("Resume", tabName="resume", icon=icon("th")),
-			menuItem("Articles", tabName="articles", icon=icon("th"))
-		)
-	),
-	dashboardBody(
-		tabItems(
-			tabItem(tabName="overall",
-				fluidRow(
-					box(plotOutput("overallTop10", height=300)),
-					box(plotOutput("overallCloud", height=300))
-				),
-				fluidRow(
-					box(plotOutput("overallEmotions", height=300)) 
-				)
-			)
-		)
-	)
-)
-
 server<-function(input, output) {
-	output$overallTop10<-renderPlot({
-		barplot(docsDf[1:10,]$freq, las=2, names.arg=docsDf[1:10,]$word, col="blue", main="Top 10 Most Occurring Words", ylab="Word Occurrences")
-	})
-	output$overallCloud<-renderPlot({
-		wordcloud(words=docsDf$word, freq=docsDf$freq, min.freq=5, max.words=100, random.order=FALSE, rot.per=0.3, colors=brewer.pal(5, "Dark2"))
-	})
-	output$overallEmotions<-renderPlot({
-		barplot(sort(colSums(prop.table(emo))), horiz=TRUE, cex.names=0.6, las=1, main="Overall Emotional Sentiment", xlab="Frequency of Occurrence(%)")
-	})
+  output$overallTop10<-renderPlot({
+    barplot(docsDf[1:10,]$freq, las=2, names.arg=docsDf[1:10,]$word, col="blue", main="Top 10 Most Occurring Words", ylab="Word Occurrences")
+  })
+  output$overallCloud<-renderPlot({
+    wordcloud(words=docsDf$word, freq=docsDf$freq, min.freq=5, max.words=100, random.order=FALSE, rot.per=0.3, colors=brewer.pal(5, "Dark2"))
+  })
+  output$overallEmotions<-renderPlot({
+    barplot(sort(colSums(prop.table(emo))), horiz=TRUE, cex.names=0.6, las=1, main="Overall Emotional Sentiment", xlab="Frequency of Occurrence(%)")
+  })
 }
 
-shinyApp(ui, server)
-
-library(rsconnect)
-deployApp()
---
+++ ui.R
@@ -0,0 +1,26 @@
+library(shiny)
+library(shinydashboard)
+
+ui<-dashboardPage(
+  dashboardHeader(title = "Mark Syphus"),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Overall Sentiment", tabName="overall", icon=icon("dashboard")),
+      menuItem("Resume", tabName="resume", icon=icon("th")),
+      menuItem("Articles", tabName="articles", icon=icon("th"))
+    )
+  ),
+  dashboardBody(
+    tabItems(
+      tabItem(tabName="overall",
+              fluidRow(
+                box(plotOutput("overallTop10", height=300)),
+                box(plotOutput("overallCloud", height=300))
+              ),
+              fluidRow(
+                box(plotOutput("overallEmotions", height=300)) 
+              )
+      )
+    )
+  )
+)

7a7f734da8b378d91fc792040d64d8969a952628
msyphus <mark.syphus@gmail.com>
2021-03-29 12:31:29 -0600

    added bar graphs and word cloud to overall page

--- textAnalysis.R
+++ textAnalysis.R
@@ -6,6 +6,7 @@ library(syuzhet)
 library(ggplot2)
 library(rvest)
 library(magrittr)
+library(RColorBrewer)
 
 ### Create text objects
 
@@ -47,3 +48,55 @@ docs<-tm_map(docs, removePunctuation)
 docs<-tm_map(docs, content_transformer(tolower))
 docs<-tm_map(docs, removeWords, stopwords("english"))
 docs<-tm_map(docs, stripWhitespace)
+
+# Create document matrix and data frame
+termDocs<-TermDocumentMatrix(docs)
+docsMat<-as.matrix(termDocs)
+docsMat<-sort(rowSums(docsMat), decreasing=TRUE)
+docsDf<-data.frame(word=names(docsMat), freq=docsMat)
+
+# Explore results
+hist(docsDf$freq)
+
+# Get sentiment data
+emo<-get_nrc_sentiment(c(resume, foodWaste, product, success, fishing, lean))
+
+# Create Dashboard
+
+ui<-dashboardPage(
+	dashboardHeader(title = "Mark Syphus"),
+	dashboardSidebar(
+		sidebarMenu(
+			menuItem("Overall Sentiment", tabName="overall", icon=icon("dashboard")),
+			menuItem("Resume", tabName="resume", icon=icon("th")),
+			menuItem("Articles", tabName="articles", icon=icon("th"))
+		)
+	),
+	dashboardBody(
+		tabItems(
+			tabItem(tabName="overall",
+				fluidRow(
+					box(plotOutput("overallTop10", height=300)),
+					box(plotOutput("overallCloud", height=300))
+				),
+				fluidRow(
+					box(plotOutput("overallEmotions", height=300)) 
+				)
+			)
+		)
+	)
+)
+
+server<-function(input, output) {
+	output$overallTop10<-renderPlot({
+		barplot(docsDf[1:10,]$freq, las=2, names.arg=docsDf[1:10,]$word, col="blue", main="Top 10 Most Occurring Words", ylab="Word Occurrences")
+	})
+	output$overallCloud<-renderPlot({
+		wordcloud(words=docsDf$word, freq=docsDf$freq, min.freq=5, max.words=100, random.order=FALSE, rot.per=0.3, colors=brewer.pal(5, "Dark2"))
+	})
+	output$overallEmotions<-renderPlot({
+		barplot(sort(colSums(prop.table(emo))), horiz=TRUE, cex.names=0.6, las=1, main="Overall Emotional Sentiment", xlab="Frequency of Occurrence(%)")
+	})
+}
+
+shinyApp(ui, server)
